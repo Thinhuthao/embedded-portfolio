0b4febbb8be2706480d59f2436fb093ef3085b86
Thinhuthao <thinhuthao12345@gmail.com>
2026-03-31 10:58:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// Scroll reveal animation
+
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+entry.target.style.opacity = 1;
+entry.target.style.transform = "translateY(0)";
+}
+
+});
+
+});
+
+
+sections.forEach(section => {
+
+section.style.opacity = 0;
+section.style.transform = "translateY(40px)";
+section.style.transition = "all 1s";
+
+observer.observe(section);
+
+});
+
+
+// Theme toggle
+
+const toggle = document.getElementById("themeToggle");
+
+toggle.onclick = () => {
+
+document.body.classList.toggle("light");
+
+};
